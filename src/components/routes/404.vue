07a931e057d3c965cf932f243ae669a2c1e25f02
Daniel Pereira <danielpp95@gmail.com>
2017-10-15 16:18:52 -0400

    404 not found added

---
+++ src/components/routes/404.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  h1.title Page not found
+</template>
+
+<style scoped>
+.title{
+  color: white;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from '@/components/routes/Home.vue'
-import Proyectos from '@/components/routes/Proyectos.vue'
+import ProyectosPer from '@/components/routes/Proyectos-per.vue'
+import PageNotFound from '@/components/routes/404.vue'
 
 const routes = [
-  {path: '/', component: Home, name: 'home'},
-  {path: '/projects', component: Proyectos, name: 'projects'}
+  { path: '/', component: Home, name: 'home' },
+  { path: '/projects', component: ProyectosPer, name: 'per' },
+  { path: '*', component: PageNotFound, name: '404' }
 ]
 
 export default routes
