19626b439af72124f4a5ace1321cb0df4f0c50e9
Daniel Pereira <danielpp95@gmail.com>
2017-10-18 22:15:44 -0400

    seccion de articulos creada

---
+++ src/components/helpers/articulos.js
@@ -0,0 +1,11 @@
+const articulos = [
+  {
+    titulo: 'Configura Visual Studio Code como editor de texto de git',
+    plataforma: 'Platzi Blog',
+    alcance: '~160',
+    link: 'https://platzi.com/git-github/tutoriales/configura-visual-studio-code-como-editor-de-texto/',
+    id: 1
+  }
+]
+
+export default articulos
