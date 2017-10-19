8f0c89c004322de9257faab0ed2dba4c06df82b1
Daniel Pereira <danielpp95@gmail.com>
2017-10-18 21:32:28 -0400

    added about page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import Proyectos from '@/components/routes/Proyectos.vue'
 import PageNotFound from '@/components/routes/404.vue'
 import Pokedex from '@/components/routes/Pokedex'
 import Certifications from '@/components/routes/Certifications'
+import About from '@/components/routes/About'
 
 const routes = [
   { path: '/', component: Home, name: 'home' },
   { path: '/projects', component: Proyectos, name: 'per' },
   { path: '/pokedex', component: Pokedex, name: 'pokedex' },
   { path: '/certifications', component: Certifications, name: 'certifications' },
+  { path: '/about', component: About, name: 'about' },
   { path: '*', component: PageNotFound, name: '404' }
 ]
 
