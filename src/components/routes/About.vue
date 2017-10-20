172b49ce39788253314519de222c81035af30f3d
Daniel Pereira <danielpp95@gmail.com>
2017-10-20 18:46:42 -0400

    soporte a fa icons

---
+++ src/components/routes/About.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.container
+  br
+  .level
+    .level-item 
+      a(href="https://github.com/danielpp95/" target="_blank")
+        i.fa.fa-github.fa-4x
+
+    .level-item 
+      a(href="https://www.linkedin.com/in/danielpp95" target="_blank")
+        i.fa.fa-linkedin.fa-4x
+
+    .level-item 
+      a(href="mailto:hola@daniel-pereira.me")
+        i.fa.fa-envelope.fa-4x
+  br
+  p as
+</template>
+
+<style scoped>
+.level, h1, p{
+  color: white;
+}
+</style>
