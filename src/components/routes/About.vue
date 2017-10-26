9d1c16216ce22961b2d0d91dda1867a4ea30c299
Daniel Pereira <danielpp95@gmail.com>
2017-10-25 22:33:03 -0400

    about ready

--- src/components/routes/About.vue
+++ src/components/routes/About.vue
@@ -14,8 +14,25 @@
       a(href="mailto:hola@daniel-pereira.me")
         i.fa.fa-envelope.fa-4x
   br
-  p as
+  .columns(v-for="p in dpAbout" :key="p.id")
+    vue-markdown.has-text-white.has-text-centered.is-size-4.column.is-8.is-offset-2 {{p.texto}}
+    
 </template>
+
+<script>
+  import VueMarkdown from 'vue-markdown'
+  import dpAbout from '@/components/helpers/about.js'
+  export default{
+    data () {
+      return ({
+        dpAbout
+      })
+    },
+    components: {
+      VueMarkdown
+    }
+  }
+</script>
 
 <style scoped>
 .level, h1, p{
