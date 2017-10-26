81c502e3913c494b20ec5702913190d96ae5e948
Daniel Pereira <danielpp95@gmail.com>
2017-10-25 22:42:36 -0400

    build

--- src/components/helpers/about.js
+++ src/components/helpers/about.js
@@ -1,7 +1,7 @@
 const about = [
   {id: 1, texto: 'Desarrollador FrontEnd, trabajo mayormente con Vue.js pero también me gusta React.'},
   {id: 2, texto: 'Actualmente trabajo en [Inversiones Manfer, C.A](http://manfer.com.ve/) como IT Manager ofreciendo soporte de producto y creando una que otra app de gestión interna.'},
-  {id: 3, texto: 'Mi pasión es el código, crear algo desde cero es cómo poder hacer magia (y lo más cercano que hay a la carta de Hogwarts que nunca llegó) y sentirse un dios creando un mundo.'},
+  {id: 3, texto: 'Mi pasión es el código, crear algo desde cero es cómo poder hacer magia (y lo más cercano que hay a la carta de Hogwarts que nunca llegó) y sentirse un Dios creando un mundo.'},
   {id: 4, texto: 'Quieres saber mas sobre mi? [Hablemos un poco!](mailto:hola@daniel-pereira.me)'}
 ]
 
